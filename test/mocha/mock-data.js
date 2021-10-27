a718c106db099bbf6a7c6969f463cf3307d82d79
Mandy Venables <mandylvenables@gmail.com>
2021-10-27 16:56:36 -0400

    Add tests for no client_id, client_secret, or token_endpoint.

---
+++ test/mocha/mock-data.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+
+exports.oAuth2Payload = {
+  client_id: '123',
+  client_secret: '456',
+  token_endpoint: `https://www.test.com`,
+  grant_type: 'client_credentials',
+  scope: ['meter'],
+};
