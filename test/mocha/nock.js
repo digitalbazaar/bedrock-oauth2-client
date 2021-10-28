df72c796b921f4f2a705c7534444cc91a2303a11
Mandy Venables <mandylvenables@gmail.com>
2021-10-28 13:10:33 -0400

    Add test for error with incorrect access token.

--- test/mocha/nock.js
+++ test/mocha/nock.js
@@ -9,6 +9,18 @@ nock('http://www.test.com')
   .post('/token')
   .reply(200, {access_token: '78910'});
 
-nock('http://www.test.com')
+nock('http://www.test.com', {
+  reqheaders: {
+    authorization: 'Bearer 78910',
+  }
+})
   .get('/gettest')
+  .reply(200, {response: 'success'});
+
+nock('http://www.test.com', {
+  reqheaders: {
+    authorization: 'Bearer 123',
+  }
+})
+  .get('/geterrortest')
   .reply(200, {response: 'success'});
