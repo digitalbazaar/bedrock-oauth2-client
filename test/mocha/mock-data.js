f90283daae0bc2feb7e2f5d819879d2051f081fe
Josh Dunn <jdunn@digitalbazaar.com>
2021-11-30 09:53:38 -0700

    Add database tests to explain performance.

--- test/mocha/mock-data.js
+++ test/mocha/mock-data.js
@@ -9,3 +9,17 @@ exports.oAuth2Payload = {
   grant_type: 'client_credentials',
   scope: ['meter']
 };
+
+exports.mockToken1 = {
+  client_id: '123',
+  client_secret: 'abc',
+  token_endpoint: 'http://www.test.com/token',
+  access_token: '123abc'
+};
+
+exports.mockToken2 = {
+  client_id: '234',
+  client_secret: 'bcd',
+  token_endpoint: 'http://www.test.com/token',
+  access_token: '234bcd'
+};
